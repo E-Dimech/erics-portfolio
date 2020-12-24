da4311e45a8d014d72db81601b441156a6df8068
Eric Dimech <ericdimech@hotmail.com>
2020-12-24 15:35:09 -0500

    Styled NavBar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+// import Computer from "../../Assets/Computer/pngfind.com-computer-screen-png-294307.png";
+import "./Home.scss";
 
 function Home() {
-  return <h1>Helllllo World</h1>;
+  return (
+    <>
+      {/* <div>
+        <img src={Computer} alt="computer monitor" className="test" />
+      </div> */}
+      <p className="testtest">Hello,</p>
+      <p className="testtest1">I'm Eric,</p>
+      <p className="testtest2">fullstack developer.</p>
+      <p className="testtest3">JavaScript / React / Express</p>
+    </>
+  );
 }
 
 export default Home;
