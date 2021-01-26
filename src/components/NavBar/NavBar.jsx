56dba99a3b42e63f1ddb79d8d74cf98c841949d8
Eric Dimech <ericdimech@hotmail.com>
2021-01-26 16:10:02 -0500

    navbar styling adjustment

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,11 +2,8 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { MenuItems } from "./MenuItems";
 
-// import Computer from "../../Assets/Computer/PinClipart.com_computer-cpu-clipart_1324075.png";
-// // import Icon from "../../Assets/Icon/E Logo.gif";
 import Logo from "../../Assets/Icon/elogo.png";
 
-// import Test from "../../Assets/Icon/E Logo (1).png";
 import "./NavBar.scss";
 
 class NavBar extends Component {
